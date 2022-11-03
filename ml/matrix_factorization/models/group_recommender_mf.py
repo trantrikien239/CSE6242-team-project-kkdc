fb859bbfa30c24add64e4149b43eb321e57fe862
Kien Tran <trantrikien239@gmail.com>
2022-11-03 15:52:17 -0400

    Implement group matrix factorization recommender

--- ml/matrix_factorization/models/group_recommender_mf.py
+++ ml/matrix_factorization/models/group_recommender_mf.py
@@ -1,17 +1,25 @@
+from turtle import shape
 import numpy as np
-from np.linalg
 from scipy.sparse import csr_matrix
 import pandas as pd
-from explicit_mf_with_bias import SGDExplicitBiasMF
+import pickle
+
+from .explicit_mf_with_bias import SGDExplicitBiasMF
 
 class GroupRecommenderMF(SGDExplicitBiasMF):
-    def __init__(self, **kwargs):
-        self.global_bias = 0 # To implement
-        self.item_embeddings = 0 # To implement, shape=(N_ITEM, D)
-        self.item_bias = 0 # To implement
-        self.n_items = self.item_embeddings
-        self.n_factors = self.item_embeddings.shape[1]
-        pass
+    def __init__(self, full_model_file_path, item_encoder_file_path):
+        with open(full_model_file_path, "rb") as f:
+            mf_full_model = pickle.load(file=f)
+        self.item_bias = mf_full_model.item_bias
+        self.item_vecs = mf_full_model.item_vecs
+        self.global_bias = mf_full_model.global_bias
+        self.n_factors = mf_full_model.item_vecs.shape[1]
+        self.item_encoder_df = pd.read_csv(item_encoder_file_path)
+        self.item_encoder_df.rename(columns={
+            "Orignal Id":"original_id",
+            "Encoded Id":"encoded_id"
+            }, inplace=True)
+    
 
     def recommend_group(self, group_rating_df, reg):
         """
@@ -22,12 +30,66 @@ class GroupRecommenderMF(SGDExplicitBiasMF):
         group_rating_df_encoded = self.item_encode(group_rating_df)
         
         virtual_user_rating = self.agg_group(group_rating_df_encoded)
+
         virtual_user_embedding, virtual_user_bias = self.train_virtual(
             virtual_user_rating, reg)
-        predicted_virtual = self.predict_vitual(virtual_user_embedding, virtual_user_bias)
-        top_10_encoded = self.sort_and_filter(predicted_virtual)
-        top_10 = self.item_decode(top_10_encoded)
+        predicted_virtual = self.predict_virtual(virtual_user_embedding, virtual_user_bias)
+        df_top_10_encoded = self.sort_and_filter(predicted_virtual, virtual_user_rating)
+        top_10 = self.item_decode(df_top_10_encoded)
         return top_10
+
+    def item_decode(self, df_top_10_encoded):
+        """
+        Input:
+            - df_top_10_encoded: (2, 10), columns = ["item_id_encoded", "rating"]
+        Output:
+            - top_10: (2, 10), columns = ["item_id", "rating"]
+        """
+        top_10 = df_top_10_encoded.merge(
+            self.item_encoder_df, 
+            left_on="item_id_encoded", 
+            right_on="encoded_id"
+            )
+        top_10 = top_10[["original_id", "rating"]]
+        top_10.rename(columns={
+            "original_id":"item_id",
+            "rating":"rating"
+            }, inplace=True)
+        top_10.sort_values(by="rating", ascending=False, inplace=True)
+        return top_10
+
+    def sort_and_filter(self, predicted_virtual, virtual_user_rating):
+        """
+        Input:
+            - predicted_virtual: (1, n_items_all)
+            - virtual_user_rating: Compressed Sparse Row matrix containing the rating 
+                shape = (n_users_in_group + 1, n_items_all)
+        Output:
+            - df_top_10_encoded: (2, 10), columns = ["item_id_encoded", "rating"]
+        """
+        predicted_virtual = predicted_virtual.flatten()
+        virtual_user_rating = virtual_user_rating.flatten()
+        predicted_virtual[virtual_user_rating > 0] = -np.inf
+        top_10_encoded = np.argsort(predicted_virtual)[-10:]
+        top_10_rating = predicted_virtual[top_10_encoded]
+        df_top_10_encoded = pd.DataFrame({
+            'item_id_encoded': top_10_encoded,
+            'rating': top_10_rating
+        })
+        return df_top_10_encoded
+
+    def predict_virtual(self, virtual_user_embedding, virtual_user_bias):
+        """`
+        Input:
+            - virtual_user_embedding: (1, n_factors)
+            - virtual_user_bias: (1,1)
+        Output:
+            - predicted_virtual: (1, n_items_all)
+        """
+        predicted_virtual = self.global_bias + virtual_user_bias + \
+            self.item_bias + virtual_user_embedding.dot(self.item_vecs.T)
+        
+        return predicted_virtual
 
     def train_virtual(self, virtual_user_rating, reg):
         """
@@ -35,29 +97,41 @@ class GroupRecommenderMF(SGDExplicitBiasMF):
             - virtual_user_rating: Compressed Sparse Row matrix containing the rating 
                 shape = (n_users_in_group + 1, n_items_all)
         """
-        n_items_in_group = 0 # num items that's rated by group users
-        s_ = 0 # Shape: (n_items_in_group, 1)
-        A_ = 0 # Shape: (n_items_in_group, n_factors + 1)
+        n_items_in_group = int((virtual_user_rating > 0).sum()) # num items that's rated by group users
+        item_indices = np.argwhere(virtual_user_rating > 0)[:,1].flatten()
+        r_ = np.array(virtual_user_rating.T[item_indices])[:,0].flatten()
+        b_i_ = self.item_bias[item_indices]
+        s_ = r_ - self.global_bias - b_i_
+        s_ = s_.reshape(-1, 1)
+        A_ = self.item_vecs[item_indices]
+        A_ = np.hstack((A_, np.ones((n_items_in_group, 1))))
+
+        # s_ = np.zeros(shape=(n_items_in_group,1)) # Shape: (n_items_in_group, 1)
+        
+        # A_ = np.zeros(shape=(n_items_in_group, self.n_factors + 1)) # Shape: (n_items_in_group, n_factors + 1)
+
         pb_g = np.linalg.inv(
             A_.T.dot(A_) + reg * np.eye(N=self.n_factors+1)
             ).dot(A_.T).dot(s_) # (n_factors + 1,1)
-        
-        p_g = pb_g[:-1,1].T # (1, n_factors)
-        b_g = pb_g[:-1,1] # (1,1)
+        p_g = pb_g[:-1,0].T # (1, n_factors)
+        b_g = pb_g[-1,0] # (1,1)
         
         return p_g, b_g
-
-        raise NotImplementedError
 
     def agg_group(self, group_rating_df_encoded):
         """
         Input: group_rating_df_encoded (3 columns: user_name, item_id_encoded, rating)
         Output: 
-            - virtual_user_rating: Compressed Sparse Row matrix containing the rating 
+            - virtual_user_rating: Numpy array containing the rating, shape = (1, num_anime_total)
         """
+        agg_df = group_rating_df_encoded.groupby("item_id")["rating"].mean()
+        num_anime_total = self.item_vecs.shape[0]
         # virtual_user_rating = csr_matrix(
-        #     (df_enc_train.rating, (df_enc_train.user_id, df_enc_train.anime_id)), 
-        #     shape=(N_USER, N_ANIME))
+        #     (agg_df.tolist(), ([0]*len(agg_df), agg_df.index)), 
+        #     shape=(1, num_anime_total))
+        virtual_user_rating = np.zeros(shape=(num_anime_total, 1))
+        virtual_user_rating[agg_df.index] = agg_df.values.reshape(-1,1)
+        virtual_user_rating = virtual_user_rating.T
         return virtual_user_rating
 
     
@@ -66,9 +140,8 @@ class GroupRecommenderMF(SGDExplicitBiasMF):
         Keep consistent with encoding, access encoding dictionary
         """
         group_df = group_rating_df.copy()
-        data_path = "../../data/"
-        encode_df = pd.read_csv(data_path + "anime_encoder.csv") 
-        encode_df = encode_df.to_dict()
-        group_rating_df["anime_id"] = group_rating_df.anime_id.apply(encode_df.get)
+        encode_df = self.item_encoder_df.set_index(
+            "original_id")["encoded_id"].to_dict()
+        group_df["item_id"] = group_df.item_id.apply(encode_df.get)
 
-        return group_rating_df
+        return group_df
